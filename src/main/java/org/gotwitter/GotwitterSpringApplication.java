975a3466848273b0ec29dbdc8ac286d3e2d5147a
Douglas Souza <douglas_souza03@hotmail.com>
2019-02-26 05:24:54 -0300

    Initial project structure

---
+++ src/main/java/org/gotwitter/GotwitterSpringApplication.java
@@ -0,0 +1,13 @@
+package org.gotwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotwitterSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotwitterSpringApplication.class, args);
+    }
+
+}
