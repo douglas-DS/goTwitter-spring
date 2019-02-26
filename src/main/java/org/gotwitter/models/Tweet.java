f3c908cbee8dd8fecbd03157dc5edf94316c5133
Douglas Souza <douglas_souza03@hotmail.com>
2019-02-26 05:35:55 -0300

    Add app properties configuration to mongo server connection

--- src/main/java/org/gotwitter/models/Tweet.java
+++ src/main/java/org/gotwitter/models/Tweet.java
@@ -1,4 +1,76 @@
 package org.gotwitter.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "tweets")
 public class Tweet {
+
+    @Id
+    private String id;
+
+    private String author;
+    private String content;
+    private Integer likes;
+    private String createdAt;
+
+    public Tweet() {
+        this.likes = 0;
+        this.createdAt = new Date().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Tweet{" +
+                "id='" + id + '\'' +
+                ", author='" + author + '\'' +
+                ", content='" + content + '\'' +
+                ", likes='" + likes + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+
 }
+
