7995380843405bc5d18340e482bf518097a3c047
Douglas Souza <douglas_souza03@hotmail.com>
2019-02-26 05:48:09 -0300

    Tweet Repository class method find by id implemented

--- src/main/java/org/gotwitter/repository/TweetRepo.java
+++ src/main/java/org/gotwitter/repository/TweetRepo.java
@@ -1,4 +1,10 @@
 package org.gotwitter.repository;
 
-public class TweetRepo {
+import org.gotwitter.models.Tweet;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TweetRepo extends MongoRepository<Tweet, String> {
+
+    Tweet findTweetById(String id);
+
 }
