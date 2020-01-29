247d9808e70e6eb5bd2ecfda8b7ac3b577994528
Douglas Souza <41555946+douglas-DS@users.noreply.github.com>
2020-01-29 17:06:25 -0300

    Update

--- src/main/java/org/gotwitter/repository/TweetRepository.java
+++ src/main/java/org/gotwitter/repository/TweetRepository.java
@@ -3,7 +3,6 @@ package org.gotwitter.repository;
 import org.gotwitter.model.Tweet;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-@Repository
 public interface TweetRepository extends MongoRepository<Tweet, String> {
     Tweet findTweetById(String id);
 }
